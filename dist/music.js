b7d4d3448fa70bfbf1796a37e310b534671a9441
yxh0714 <1817892092@qq.com>
2020-05-11 23:38:09 +0800

    Site updated: 2020-05-11 23:38:03

---
+++ dist/music.js
@@ -0,0 +1,75 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    theme: '#000000',
+    preload: "auto",
+    autoplay: false,
+    audio: [
+  {
+        name: 'Just Like Fire',
+        artist: 'P!nk',
+        url: 'http://music.163.com/song/media/outer/url?id=409931224.mp3',
+        cover: 'http://p2.music.126.net/TK0M1WNzgGfydYTRPe5sDA==/18785156162534586.jpg'
+      },
+ {
+        name: '独家记忆',
+        artist: '陈小春',
+        url: 'http://music.163.com/song/media/outer/url?id=63650.mp3',
+        cover: 'http://p2.music.126.net/D_e8zPgwkKCk1uSF-HSgbw==/18806046883327256.jpg'
+      },
+  {
+        name: '说谎',
+        artist: '林宥嘉',
+        url: 'http://music.163.com/song/media/outer/url?id=108390.mp3',
+        cover: 'http://p1.music.126.net/mMZNB-jhYsw29K61QtopJA==/109951163187404137.jpg'
+      },
+  {
+        name: '喜欢你',
+        artist: 'G.E.M.邓紫棋',
+        url: 'http://music.163.com/song/media/outer/url?id=28949444.mp3',
+        cover: 'http://p1.music.126.net/u_1EudmF8Swgow6vfgYe1g==/8896148580676276.jpg'
+      },
+  {
+        name: '趁早',
+        artist: '张惠妹',
+        url: 'http://music.163.com/song/media/outer/url?id=326997.mp3',
+        cover: 'http://p1.music.126.net/WoybHfawR7c1gmXJQDHviw==/32985348845360.jpg'
+      },
+  {
+        name: '爱情陷阱',
+        artist: '谭咏麟',
+        url: 'http://music.163.com/song/media/outer/url?id=4871718.mp3',
+        cover: 'http://p2.music.126.net/QigStkElP-yXC8H8ibnGog==/109951164900283582.jpg?'
+      },
+  {
+        name: 'GIANTS(真实伤害)',
+        artist: '英雄联盟',
+        url: 'http://music.163.com/song/media/outer/url?id=1404306877.mp3',
+        cover: 'http://p1.music.126.net/CKDoRspxqS3rOTZFpqS3Zg==/109951164497509449.jpg'
+      },
+ {
+        name: '夏日寒风',
+        artist: '谭咏麟',
+        url: 'http://music.163.com/song/media/outer/url?id=152283.mp3',
+        cover: 'http://p1.music.126.net/0g5XXOn-4BeBv2J4m-wXpw==/661905999933308.jpg'
+      },
+ {
+        name: '爱上张无忌',
+        artist: '毛啊敏',
+        url: 'http://music.163.com/song/media/outer/url?id=166321.mp3',
+        cover: 'http://p2.music.126.net/EN2oaemrTlTvP-rlo5dP2A==/86861418607028.jpg'
+      },
+ {
+        name: '两两相忘',
+        artist: '熙兮兮兮',
+        url: 'http://music.163.com/song/media/outer/url?id=1315915479.mp3',
+        cover: 'http://p2.music.126.net/GvidEH5My1KvAm3pFD-Jxg==/109951163424532980.jpg'
+      },
+ {
+        name: 'Ｋ.Ｉ.Ｓ.Ｓ.',
+        artist: 'bl00dwave',
+        url: 'http://music.163.com/song/media/outer/url?id=28522035.mp3',
+        cover: 'http://p1.music.126.net/OiP7XMTbARGvHRQOlyUHCA==/5989039836716824.jpg'
+      }
+    ]
+});
